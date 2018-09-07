f7571ec5ef03315aa1eb33f4b9a0fdd1da75fa07
edwinlawisan <edwinlawisan123@gmail.com>
2018-09-07 16:05:18 +0700

    Initial Commit Dosen.java

---
+++ Dosen.java
@@ -0,0 +1,20 @@
+class Dosen {
+    private String nama;
+    private String mataKuliah;
+
+    public void setNama(String nama){
+        this.nama = nama;
+    }
+
+    public void setMataKuliah(String mataKuliah){
+        this.mataKuliah = mataKuliah;
+    }
+
+    public String getNama(){
+        return this.nama;
+    }
+
+    public String getMataKuliah(){
+        return this.mataKuliah;
+    }
+}
