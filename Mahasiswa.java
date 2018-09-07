b493da3efa242c553aac0d96d2f448f9206275f5
edwinlawisan <edwinlawisan123@gmail.com>
2018-09-07 16:01:09 +0700

    Initial Commit Mahasiswa.java

---
+++ Mahasiswa.java
@@ -0,0 +1,20 @@
+class Mahasiswa {
+    private String nama;
+    private String npm;
+
+    public void setNama(String nama){
+        this.nama = nama;
+    }
+
+    public void setNPM(String npm){
+        this.npm = npm;
+    }
+
+    public String getNama(){
+        return this.nama;
+    }
+
+    public String getNPM(){
+        return this.npm;
+    }
+}
